fe29dcc95d6835d4e23a150033bc5b68f22bcda2
Abundiz <3duabundiz@gmail.com>
2020-04-25 12:45:14 -0500

    Agregar a la base de datos

--- src/screens/peticiones/addPetition.js
+++ src/screens/peticiones/addPetition.js
@@ -1,15 +1,15 @@
 import React,{Component} from 'react';
 import {View,Text,StyleSheet,ScrollView,Switch,} from 'react-native';
 import {Input,Button } from 'react-native-elements';
-
+import DatePicker from 'react-native-datepicker'
 class AddPetition extends Component{    
     state ={
         name:'', email:'', city: '',
-        category:'',time:'',petition:''
+        category:'',date:'',petition:''
     }
     //#region setStateFunctions
-    changeName=(inputText) => {
-        this.setState({name:inputText});
+    changeName=(inputText) => {        
+        this.setState({name:inputText});        
       }
       changeEmail=(inputText) => {
         this.setState({email:inputText});
@@ -20,24 +20,26 @@ class AddPetition extends Component{
       changeCategory=(inputText) => {
         this.setState({category:inputText});
       }
-      changeTime=(inputText) => {
-        this.setState({time:inputText});
+      changeDate=(inputText) => {
+        this.setState({date:inputText});
       }
       changePetition=(inputText) => {
-        this.setState({petition:inputText});
+        this.setState({petition:inputText});        
       }
     //#endregion
     sendPetition = () =>{
+
+        console.log(this.state.name+"&email="+this.state.email+"&city="+this.state.category+"&duration="+this.state.date+
+        "&petition="+this.state.petition)
+        
         var xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function() {
-  
-  
+        xhttp.onreadystatechange = function() {            
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:       
        if(xhttp.responseText == "1"){
-       Alert.alert(
-         "Mensaje",
-         "Se insertó con exito en la base de datos",
+       alert(
+         "Gracias por su petición",
+         "Estaremos orando por esta petición confíe en Dios",
          [
            {text: 'ok'},
          ],
@@ -45,9 +47,15 @@ class AddPetition extends Component{
 
        );
        }
+       else{
+           alert("nope")
+       }
     }    
 };
-xhttp.open("GET", "https://unshorn-flares.000webhostapp.com/altas.php?codigo="+this.state.codigo+"&nombre="+this.state.nombre+"&carrera="+this.state.carrera+"&centro="+this.state.centro, true);
+xhttp.open("GET", "https://unshorn-flares.000webhostapp.com/uploadPetition.php?name="+
+this.state.name+"&email="+this.state.email+"&city="+this.state.city+"&category="+this.state.category+
+"&duration="+this.state.date+"&petition="+this.state.petition,
+true);
 xhttp.send();
 
     }
@@ -74,20 +82,50 @@ xhttp.send();
                     label = "Correo electrónico"
                     placeholder ="example@outlook.com"
                     keyboardType = "email-address"
+                    onChangeText={this.changeEmail}
                 />
                 <Input 
                     containerStyle= {styles.container}
                     labelStyle = {styles.label}
                     label = "Ciudad"
                     placeholder ="Guadalajara"
+                    onChangeText={this.changeCity}
                     
                 />
                 <Input 
                     containerStyle= {styles.container}
                     labelStyle = {styles.label}
                     label = "Categoria:"
-                    placeholder ="Sanidad"                    
+                    placeholder ="Sanidad"   
+                    onChangeText={this.changeCategory}                 
                 />
+                <View style={styles.center}>
+                <Text style = {styles.label}>Fecha de eliminación</Text>
+                <DatePicker
+                    style={{width: 200}}
+                    date={this.state.date}
+                    mode="date"
+                    placeholder="select date"
+                    format="YYYY-MM-DD"
+                    minDate="2020-04-25"
+                    maxDate="2020-05-02"
+                    confirmBtnText="Confirm"
+                    cancelBtnText="Cancel"
+                    customStyles={{
+                    dateIcon: {
+                        position: 'absolute',
+                        left: 0,
+                        top: 4,
+                        marginLeft: 0
+                    },
+                    dateInput: {
+                        marginLeft: 36
+                    }
+                    // ... You can check the source to find the other keys.
+                    }}
+                    onDateChange={(date) => {this.setState({date: date})}}
+                />
+                </View>
                 <Input 
                     containerStyle= {styles.container}
                     labelStyle = {styles.label}
@@ -102,6 +140,7 @@ xhttp.send();
                     label = "Su petición "
                     placeholder ="Escriba la petición por la cual quiere que oremos (Max 400 caracteres)"                                        
                     multiline = {true}
+                    onChangeText={this.changePetition}
                     
                 />
                     <View style= {styles.center}>
@@ -118,6 +157,7 @@ xhttp.send();
                 titleStyle = {styles.titleButton}
                 title = "Registrar"                
                 buttonStyle = {styles.buttonStyle}
+                onPress ={this.sendPetition}
                 
                 />
             </View>
